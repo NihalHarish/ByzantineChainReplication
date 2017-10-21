29d658912b0662314dd594994704e66c6eba64aa
nirvik1993 <nirvik1993@gmail.com>
2017-10-21 15:05:12 -0400

    pushing constants

---
+++ src/constants.py
@@ -0,0 +1,8 @@
+RESULT_SHUTTLE_FAILURE_TRIGGER = 'result_shuttle'
+CLIENT_REQUEST_FAILURE_TRIGGER = 'client_request'
+SHUTTLE_FAILURE_TRIGGER = 'shuttle'
+FORWARDED_FAILURE_TRIGGER = 'forwarded_request'
+
+CHANGE_OPERATION_FAILURE = 'change_operation'
+CHANGE_RESULT_FAILURE = 'change_result'
+DROP_RESULT_FAILURE = 'drop_result_stmt'
