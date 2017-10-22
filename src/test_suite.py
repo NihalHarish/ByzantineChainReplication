c019f2a3ab1d9d5d25981b426c2109187b8e4adf
Nihal Harish <nihal42harish@gmail.com>
2017-10-21 21:03:24 -0400

    failures

--- src/test_suite.py
+++ src/test_suite.py
@@ -1,19 +1,38 @@
 import config_handler
 import json
-
+from optparse import OptionParser
+import sys
 
 if __name__ == '__main__':
-    test_value = json.loads(config_handler.load_config()['test1']['test_dict'])
-    data = None
-    with open('test.json') as test_file:    
-        data = json.load(test_file)
-    if data is not None:
-        if data == test_value:
-            print('Verified')
-        else:
-            print('Data: ' + str(data) + str(type(data)))
-            print('Test: ' + str(test_value) + str(type(test_value)))
-            print('Did not match')
 
+    option_parser = OptionParser()
+    option_parser.add_option("--test_case_name", dest='test_case',help = 'test case name in the config file that should be tested', metavar='TESTCASE')
+    (options, args) = option_parser.parse_args()
+    test_case = None
+
+    if not options.test_case:
+        print('Print Test Case Name Not Supplied')
+        sys.exit(0)
     else:
-        print('data is None?!')
+        test_case = options.test_case
+
+    
+    test_value = json.loads(config_handler.load_config()[test_case]['test_dict'])
+    client_data = None
+    with open('test.json') as test_file:    
+        client_data = json.load(test_file)
+    if client_data is not None:
+        if client_data == test_value:
+            pass
+        else:
+            print('Verification failed')
+            sys.exit(0)
+    for key in test_value:
+        if key in client_data:
+            print('checking key: ' +str(key)+ " for value: " + str(test_value[key]))
+            if client_data[key] == test_value[key]:
+                print('Verified')
+            else:
+                print('Verification Failed')
+        else:
+            print('Verification Failed')
