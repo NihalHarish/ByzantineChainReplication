0b1dd47bd6c79aba1b5421fa7c66fb3db178240e
nirvik1993 <nirvik1993@gmail.com>
2017-11-14 21:49:05 -0500

    lot of work

---
+++ src/main.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# python3 -m da -n MainNode -D main.da 
+python3 -m da -n MainNode main.da 
---
+++ src/olympus.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+python3 -m da -n OlympusNode -D olympus.da
+python3 -m da -n OlympusNode olympus.da
